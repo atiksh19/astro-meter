50d0b3f9f13245e94a51e29a489c94d9e8df843d
Atiksh <atiksh1909@gmail.com>
2021-11-20 21:05:42 +0530

    create structure

--- index.js
+++ index.js
@@ -1,16 +1,70 @@
-/* loading data */
-var data = [];
-$.getJSON("data.json", (json) => {
-    data = json["planets"]
-});
-/* loading data */
-
-/* functions */
-// function change(i){}
-/* functions */
+/* data */
+planets = [
+    {
+        "name": "Mercury",
+        "img": "",
+        "basic-info": {
+            "discovery": "2014",
+            "mass": "0.055 Earths",
+            "orbital radius": "0.432 AU",
+            "orbital period": "87.97 Earth days"
+        },
+        "years-conversion": 0,
+        "weight-conversion": 0
+    },
+    {
+        "name": "",
+        "img": "",
+        "basic-info": {
+            "discovery": "",
+            "mass": "",
+            "orbital radius": "",
+            "orbital period": ""
+        },
+        "years-conversion": 0,
+        "weight-conversion": 0
+    },
+    {
+        "name": "",
+        "img": "",
+        "basic-info": {
+            "discovery": "",
+            "mass": "",
+            "orbital radius": "",
+            "orbital period": ""
+        },
+        "years-conversion": 0,
+        "weight-conversion": 0
+    },
+    {
+        "name": "",
+        "img": "",
+        "basic-info": {
+            "discovery": "",
+            "mass": "",
+            "orbital radius": "",
+            "orbital period": ""
+        },
+        "years-conversion": 0,
+        "weight-conversion": 0
+    }
+];
+/* data */
 
 /* preloader */
 window.addEventListener("load", () => {
     $("#preloader").css("z-index", -2);
 });
 /* preloader */
+
+/* functions */
+function changePlanet(n){
+    $("#planetimg").attr("src", planets[n]["img"])
+    $("#planet-name").text(planets[n]["name"]);
+    info = `discovery: ${planets[n]["basic-info"]["discovery"]}<br>
+            mass: ${planets[n]["basic-info"]["mass"]}<br>
+            orbital radius: ${planets[n]["basic-info"]["orbital radius"]}<br>
+            orbital period: ${planets[n]["basic-info"]["orbital period"]}`
+    $("#planet-info").html(info);
+}
+/* functions */
